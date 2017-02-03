52fd4b59e6a5e02d41a9f0f63a197d4b3cda7d13
sharkbite1414 <sharkbite1414@gmail.com>
2017-02-03 15:48:17 +0000

    Added prompt to change grid size and refactored script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,38 @@
 $(document).ready(function(){
-  for (i=0; i<16; i++) {
-    $("#area").append("<div class='box'></div>");
-  };
+  // initilise grid
+  var gridSize=16;
+  makeGrid(gridSize);
+
+  // hover effect
   $(".box").hover(function(){
     $(this).addClass("selected");
   });
+
+  // reset grid color
   $("#reset").click(function(){
-    $(".box").removeClass("selected");
+    resetCol();
+  });
+
+  // Change grid size button
+  $("#size").click(function(){
+    gridSize = Number(prompt("Enter grid size","16"));
+    makeGrid(gridSize)
   });
 });
+
+// Functions
+// Set grid size
+function makeGrid(gridSize) {
+  resetCol();
+  for (i=0; i<gridSize; i++) {
+    $("#area").append("<div class='row" + i +"''></div>")
+    for (j=0; j<gridSize; j++) {
+      $(".row" +i).append("<div class='box'></div>");
+    };
+  };
+};
+
+// Reset grid color
+function resetCol() {
+  $(".selected").removeClass("selected");
+}
